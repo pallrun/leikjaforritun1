3cb01bceb1c094abc5d113c90ff8dcf41b0d545d
palliking <palliking@protonmail.com>
2022-11-22 10:48:11 +0000

    Add files via upload

---
+++ Verkefni3/Scripts/bullet.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class bullet : MonoBehaviour
+{
+    public float speed=20f;
+    public Rigidbody rb;
+    public GameObject medkit;
+    void Start()
+    {
+        rb.velocity = transform.forward * speed;
+        //countText = GameObject.Find("lives").GetComponent<Text>();
+    }
+    private void OnTriggerEnter(Collider collision)
+    {
+        
+        if (collision.tag=="goon")
+        {
+            Destroy(collision.gameObject);//eyðir kassanum           
+            Medkit(); //spawna medkit þegar ovinur er drepinn
+        }
+       
+    }
+    void Medkit()
+    {
+        Instantiate(medkit, transform.position, new Quaternion(0, 0 , 0, 1));
+    }
+}
