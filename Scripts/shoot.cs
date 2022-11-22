e1382349630edfa821ec5d681e6c93910a8a9d06
palliking <palliking@protonmail.com>
2022-11-22 10:46:15 +0000

    Add files via upload

---
+++ Scripts/shoot.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shoot : MonoBehaviour
+{
+    public GameObject bullet;
+    public float speed = 100f;
+    
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {     
+            GameObject instBullet = Instantiate(bullet, transform.position, transform.rotation) as GameObject;
+            Rigidbody instBulletRigidbody = instBullet.GetComponent<Rigidbody>();
+            instBulletRigidbody.AddForce(transform.forward * speed);
+            Destroy(instBullet, 0.5f);//kúl eytt eftir 0.5sek
+        }
+    }
+}
+//breytti engu her
